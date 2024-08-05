8a08e82561631334c087af3fd3d4c2462d0ac7c4
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2024-08-05 23:18:41 +0900

    作業コミット

---
+++ Unity/RogueLike/Assets/Script/Base/BaseStats.cs
@@ -0,0 +1,163 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+// 追加ステータス
+public enum BonusType
+{
+    // 定数で追加
+    Bonus,
+    // %で追加
+    Boost,
+}
+
+public enum StatsType
+{
+    Attack,
+    Defence,
+    MoveSpeed,
+    HP,
+    MaxHP,
+    XP,
+    MaxXP,
+    PickUpRange,
+    AliveTime,
+    // 武器生成用の拡張タイプ
+    SpawnCount,
+    SpawnTimerMin,
+    SpawnTimerMax,
+}
+
+[SerializeField]
+public class BonusStats
+{
+    // 追加タイプ
+    public BonusType Type;
+    // 追加するプロパティ
+    public StatsType Key;
+    // 追加する値
+    public float Value;
+}
+
+// キャラクターと武器で共通のステータス
+public class BaseStats
+{
+    // Inspector上で表示されるタイトル
+    public string Title;
+    //データID
+    public int Id;
+    // 設定レベル
+    public int Lv;
+    // 名前
+    public string Name;
+    // 説明文
+    [TextArea]public string Description;
+    // 攻撃力
+    public float Attack;
+    // 防御力
+    public float Defence;
+    // HP
+    public float HP;
+    //最大HP
+    public float MaxHP;
+    // 経験値
+    public float XP;
+    // 最大経験値
+    public float MaxXP;
+    // 移動速度
+    public float MoveSpeed;
+    // 経験値取得距離
+    public float PickUpRange;
+    // 生存時間
+    public float AliveTime;
+
+    // StatsTypeとの紐づけ　インデクサを利用する
+    public float this[StatsType key]
+    {
+        get => key switch
+        {
+            StatsType.Attack => Attack,
+            StatsType.Defence => Defence,
+            StatsType.MoveSpeed => MoveSpeed,
+            StatsType.HP => HP,
+            StatsType.MaxHP => MaxHP,
+            StatsType.XP => XP,
+            StatsType.MaxXP => MaxXP,
+            StatsType.PickUpRange => PickUpRange,
+            StatsType.AliveTime => AliveTime,
+            _ => 0
+        };
+        set
+        {
+            switch (key)
+            {
+                case StatsType.Attack:
+                    Attack = value;
+                    break;
+                case StatsType.Defence:
+                    Defence = value;
+                    break;
+                case StatsType.MoveSpeed:
+                    MoveSpeed = value;
+                    break;
+                case StatsType.HP:
+                    HP = value;
+                    break;
+                case StatsType.MaxHP:
+                    MaxHP = value;
+                    break;
+                case StatsType.XP:
+                    XP = value;
+                    break;
+                case StatsType.MaxXP:
+                    MaxXP = value;
+                    break;
+                case StatsType.PickUpRange:
+                    PickUpRange = value;
+                    break;
+                case StatsType.AliveTime:
+                    AliveTime = value;
+                    break;
+            }
+        }
+    }
+
+    // ボーナス値
+    protected float applyBonus(float currentValue, float value, BonusType type)
+    {
+        // 固定値を加算
+        if (BonusType.Bonus == type)
+        {
+            return currentValue + value;
+        }
+        // %で加算
+        else if (BonusType.Boost == type)
+        {
+            return currentValue * (1 + value);
+        }
+        return currentValue;
+    }
+
+    // ボーナス追加
+    protected void addBonus(BonusStats bonus)
+    {
+        float value = applyBonus(this[bonus.Key], bonus.Value, bonus.Type);
+        // 最大値があるもの
+        if (StatsType.HP == bonus.Key)
+        {
+            value = Mathf.Clamp(value, 0, MaxHP);
+        }
+        else if (StatsType.XP == bonus.Key)
+        {
+            value = Mathf.Clamp(value, 0, MaxXP);
+        }
+
+        this[bonus.Key] = value;
+    }
+
+    // コピーしてデータを返す
+    public BaseStats GetCopy()
+    {
+        return (BaseStats)MemberwiseClone();
+    }
+}
+
---
+++ Unity/RogueLike/Assets/Script/GameSceneDirector.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GameSceneDirector : MonoBehaviour
+{
+    //　タイルマップ
+    [SerializeField] GameObject grid;
+    [SerializeField] Tilemap tilemapCollider;
+
+    // マップ全体座標
+    // タイルマップ
+    public Vector2 TileMapStart = new Vector2(float.MaxValue, float.MaxValue);
+    public Vector2 TileMapEnd = new Vector2(float.MinValue, float.MinValue);
+    // プレイヤー
+    public Vector2 WorldStart;
+    public Vector2 WorldEnd;
+    // カメラ
+    public Vector2 CamSideStart = new Vector2(float.MaxValue, float.MaxValue);
+    public Vector2 CamSideEnd = new Vector2(float.MinValue, float.MinValue);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // カメラの移動できる範囲
+        foreach (Transform item in grid.GetComponentInChildren<Transform>())
+        {
+
+            Vector3 pos = item.position;
+            // 開始位置（左下の角）
+            TileMapStart.x = Mathf.Min(TileMapStart.x, pos.x);
+            TileMapStart.y = Mathf.Min(TileMapStart.y, pos.y);
+
+            // 終了位置（右上の角）
+            TileMapEnd.x = Mathf.Max(TileMapEnd.x, pos.x);
+            TileMapEnd.y = Mathf.Max(TileMapEnd.y, pos.y);
+
+            #region
+            //// 開始位置
+            //if (TileMapStart.x > item.position.x)
+            //{
+            //    TileMapStart.x = item.position.x;
+            //}
+            //if (TileMapStart.y > item.position.y)
+            //{
+            //    TileMapStart.y = item.position.y;
+            //}
+            //// 終了位置
+            //if (TileMapEnd.x < item.position.x)
+            //{
+            //    TileMapEnd.x = item.position.x;
+            //}
+            //if (TileMapEnd.y < item.position.y)
+            //{
+            //    TileMapEnd.y = item.position.y;
+            //}
+            #endregion
+        }
+        // 結果の表示
+        Debug.Log("TileMap Start (左下の角): " + TileMapStart);
+        Debug.Log("TileMap End (右上の角): " + TileMapEnd);
+        // 使ってたやつ
+        #region
+        // 画面縦半分の描画範囲（デフォ5タイル）
+        //float cameraSize = Camera.main.orthographicSize;
+        // 画面縦横比（16:9想定）
+        //float aspect = (float)Screen.width / (float)Screen.height;
+        //print("aspect:"+ aspect);
+        // プレイヤーの移動できる範囲
+        #endregion
+
+        WorldStart = new Vector2(TileMapStart.x * 1.5f, TileMapStart.y * 1.5f);
+        WorldEnd = new Vector2(TileMapEnd.x * 1.5f, TileMapEnd.y * 1.5f);
+        print("WorldStart : " + WorldStart);
+        print("WorldEnd : " + WorldEnd);
+
+        camCorners();
+        print("camSideStart : " + CamSideStart);
+        print("camSideEnd : " + CamSideEnd);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// カメラの視錐台から表示範囲取得
+    /// </summary>
+    void camCorners()
+    {
+        // カメラの近クリップ平面までの距離
+        float nearClipPlane = Camera.main.nearClipPlane;
+
+        // カメラの遠クリップ平面までの距離
+        float farClipPlane = Camera.main.farClipPlane;
+        
+        // カメラの視野角
+        float fieldOfView = Camera.main.fieldOfView;
+        
+        // カメラのアスペクト比
+        float aspect = Camera.main.aspect;
+        
+        // 近クリップ平面のコーナーポイントを格納するための配列
+        Vector3[] nearCorners = new Vector3[4];
+        
+        // 遠クリップ平面のコーナーポイントを格納するための入れる
+        Vector3[] farCorners = new Vector3[4];
+        
+        // カメラの近クリップ平面のコーナーポイント計算
+        Camera.main.CalculateFrustumCorners(new Rect(0, 0, 1, 1), nearClipPlane, Camera.MonoOrStereoscopicEye.Mono, nearCorners);
+        
+        // カメラの遠クリップ平面のコーナーポイントを計算
+        Camera.main.CalculateFrustumCorners(new Rect(0, 0, 1, 1), farClipPlane, Camera.MonoOrStereoscopicEye.Mono, farCorners);
+        
+        /*print("nearCorners : " + nearCorners);
+          print("farCorners : " + farCorners);*/
+        
+        // カメラの位置を取得
+        Vector3 cameraPos = Camera.main.transform.position;
+        
+        // ワールド座標系での視錐台のコーナーポイントを計算
+        for (int i = 0; i < 4; i++)
+        {
+            nearCorners[i] = Camera.main.transform.TransformPoint(nearCorners[i]);
+            farCorners[i] = Camera.main.transform.TransformPoint(farCorners[i]);
+        }
+        
+        /*print("nearCorners : " + nearCorners);
+          print("farCorners : " + farCorners);*/
+        
+        foreach (Vector3 corner in farCorners)
+        {
+            // 開始位置
+            if (CamSideStart.x > corner.x)
+            {
+                CamSideStart.x = corner.x;
+            }
+            if (CamSideStart.y > corner.y)
+            {
+                CamSideStart.y = corner.y;
+            }
+            // 終了位置
+            if (CamSideEnd.x < corner.x)
+            {
+                CamSideEnd.x = corner.x;
+            }
+            if (CamSideEnd.y < corner.y)
+            {
+                CamSideEnd.y = corner.y;
+            }
+        }
+    }
+}
