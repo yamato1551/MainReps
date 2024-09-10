be65adf2e03e7ebf5bd56cd5c51c4cee3b7542e7
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2024-09-10 09:10:35 +0900

    作業コミット

---
+++ Unity/RogueLike/Assets/SlashController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashController : BaseWeapon
+{
+    // ƒgƒŠƒK[‚ªÕ“Ë‚µ‚½
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        attackEnemy(collision);
+    }
+}
