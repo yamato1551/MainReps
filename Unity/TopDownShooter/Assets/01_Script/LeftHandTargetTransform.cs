cb89afa5c010f58cb8d8732e8ee85fd9ed02895c
yamato1551 <47923511+yamato1551@users.noreply.github.com>
2024-12-09 21:18:56 +0900

    TopDownShooter 左手の調整

---
+++ Unity/TopDownShooter/Assets/01_Script/LeftHandTargetTransform.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class LeftHandTargetTransform : MonoBehaviour
+{
+    // ゲーム・オブジェクトを検出するには、このスクリプトが必要
+
+    // 左手ターゲットの位置と回転を検出
+}
--- Unity/TopDownShooter/Assets/01_Script/WeaponVisualController.cs
+++ Unity/TopDownShooter/Assets/01_Script/WeaponVisualController.cs
@@ -10,31 +10,42 @@ public class WeaponVisualController : MonoBehaviour
     [SerializeField] private Transform shotgun;
     [SerializeField] private Transform rifle;
 
+    private Transform currentGun;
+
+    [Header("Left hand IK")]
+    [SerializeField] private Transform leftHand;
+
     private void Start()
     {
-        
+        SwitchOn(pistol);
     }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
-           SwicthOn(pistol);
+           SwitchOn(pistol);
         if (Input.GetKeyDown(KeyCode.Alpha2))
-            SwicthOn(revolver);
+            SwitchOn(revolver);
         if (Input.GetKeyDown(KeyCode.Alpha3))
-            SwicthOn(autoRifile);
+            SwitchOn(autoRifile);
         if (Input.GetKeyDown(KeyCode.Alpha4))
-            SwicthOn(shotgun);
+            SwitchOn(shotgun);
         if (Input.GetKeyDown(KeyCode.Alpha5))
-            SwicthOn(rifle);
+            SwitchOn(rifle);
 
     }
 
-    // èeäÌÇÃêÿÇËë÷Ç¶
-    private void SwicthOn(Transform transform)
+    /// <summary>
+    /// èeäÌÇÃêÿÇËë÷Ç¶
+    /// </summary>
+    /// <param name="gunTransfome"></param>
+    private void SwitchOn(Transform gunTransfome)
     {
         SwitchOffGuns();
-        transform.gameObject.SetActive(true);
+        gunTransfome.gameObject.SetActive(true);
+        currentGun = gunTransfome;
+
+        AttachLeftHand();
     }
 
     /// <summary>
@@ -47,6 +58,17 @@ public class WeaponVisualController : MonoBehaviour
         {
             gunTransforms[i].gameObject.SetActive(false);
         }
+    }
+
+    /// <summary>
+    /// ç∂éËÇÃà íuí≤êÆ
+    /// </summary>
+    private void AttachLeftHand()
+    {
+        Transform targetTransform = currentGun.GetComponentInChildren<LeftHandTargetTransform>().transform;
+
+        leftHand.localPosition = targetTransform.localPosition;
+        leftHand.localRotation = targetTransform.localRotation;
     }
 
 }
